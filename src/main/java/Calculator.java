0ee74aa8635591d0233a3076fe8c81ff1ed27322
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-23 17:50:45 +0100

    Refactors calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,7 @@ public class Calculator {
     private double number1;
     private double number2;
 
-    public Calculator(){
-        this.number1 = number1;
-        this.number2 = number2;
-    }
+    public Calculator(){}
 
     public double add(double number1, double number2){
         return number1 + number2;
