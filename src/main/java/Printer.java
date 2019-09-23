6cf1d511cb5c0a04c316f5b511d3845d70f5e897
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-23 16:53:16 +0100

    Sets up printer class and tests getter

---
+++ src/main/java/Printer.java
@@ -0,0 +1,18 @@
+public class Printer {
+
+    private double sheetsLeft;
+
+
+    public Printer(){
+        this.sheetsLeft = 100;
+    }
+
+    public double getSheetsLeft() {
+        return sheetsLeft;
+    }
+
+    public void setSheetsLeft(double sheetsLeft) {
+        this.sheetsLeft = sheetsLeft;
+    }
+
+}
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer();
+    }
+
+    //has number of sheets property
+
+    @Test
+    public void hasSheetsLeft(){
+        assertEquals(100, printer.getSheetsLeft(), 0.01);
+    }
+
+
+//    Add a method to print that takes in a number of pages and number of copies.
+//    The print method will only run if the printer has enough paper. If it runs it will reduce the value of the paper left by number of copies * number of pages.
+//    Add a toner volume property to the class.
+//    Modify the printer so that it reduces the toner by 1 for each page printed.
 }
